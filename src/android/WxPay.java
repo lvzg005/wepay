72ef4cd6b6953ce1ee8f3fa822ec1dc0b6493a3e
lvzg005 <lvzg.005@gmail.coom>
2015-05-29 16:36:54 +0800

    d

--- src/android/WxPay.java
+++ src/android/WxPay.java
@@ -82,12 +82,13 @@ public class WxPay extends CordovaPlugin {
 			return false;
 		}
 		
-		/*
+		
 		final JSONObject params = args.getJSONObject(0);
 		if (params == null) {
 			callbackContext.error("params is null.");
 			return false;
 		}
+		/*
 		String ipAddress = params.getString("ipAddress");
 		//同一订单接口
 		//Map<String,String> resultUnifiedorder = unifiedorder(ipAddress);
